aaafd747271443f4e3bcab1f8a4fcc47c213a6ce
dina2803 <dg734939@gmail.com>
2023-10-14 22:09:59 +0600

    dz3_dina_29_01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      newTask: "",
+    };
+  }
+
+  createTodo = () => {
+    if (this.state.newTask === "") {
+      alert("Пустую строку нельзя создать");
+      return;
+    } else {
+      const tasks = this.state.tasks.slice();
+      tasks.push(this.state.newTask);
+      this.setState({ tasks, newTask: "" });
+    }
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>Todo list</h1>
+          <input
+              type="text"
+              value={this.state.newTask}
+              onChange={(event) => this.setState({ newTask: event.target.value })} //подглядела в интернете, не знаю можно ли так
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  this.createTodo();
+                }
+              }}
+          />
+          <button onClick={this.createTodo}>Add Task</button>
+          <ul>
+            {this.state.tasks.map((task, index) => (
+                <li key={index}>{task}</li>
+            ))}
+          </ul>
+        </div>
+    );
+  }
 }
 
 export default App;
+
